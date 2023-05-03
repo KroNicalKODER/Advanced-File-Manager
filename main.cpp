e0a0811b61f3d995ccf7676506792b937002a134
ADESH GUPTA <adeshgupta1226@gmail.com>
2023-05-04 03:22:26 +0530

    adesh

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include "huffman.h"
-#include "Lempel.h"
+#include<huffman.h>
+#include <Lempel.h>
+#include<file_copy.h>
 
 using namespace std;
 using namespace Huffman;
@@ -7,14 +8,14 @@ using namespace Huffman;
 
 int main() {
 
-    string input_file = "";
-    string output_file = "";
+    string input_file = "test.txt";
+    string output_file = "test12.dat";
 
-    compress(input_file, output_file);
+    compress( input_file,output_file );
 
     string output_text_file = output_file.substr(0, output_file.find_last_of(".")) + ".txt";
 
-    decompress(output_file, output_text_file);
+    // decompress("test.txt", "test12.");
 
     cout << "Compression and decompression completed successfully!\n";
     return 0;
